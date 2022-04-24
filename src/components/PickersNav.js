a3368b7b360d709a5a7e4529eda8ca8cb9750f88
khen0071 <71557029+khen0071@users.noreply.github.com>
2022-04-25 00:21:52 +0800

    second commit

--- src/components/PickersNav.js
+++ src/components/PickersNav.js
@@ -6,34 +6,34 @@ import { LinkContainer } from 'react-router-bootstrap';
 const PickersSheet = () => {
   return (
     <>
-      <div>
+      <div className='pickers_nav_container'>
         <Container>
           <Nav fill variant='tabs' defaultActiveKey='/'>
-            <Nav.Item>
+            <Nav.Item className='pickers_nav_item'>
               <LinkContainer to='/monday_pickers'>
                 <Nav.Link>Monday</Nav.Link>
               </LinkContainer>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item className='pickers_nav_item'>
               <LinkContainer to='/tuesday_pickers'>
                 <Nav.Link>Tuesday</Nav.Link>
               </LinkContainer>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item className='pickers_nav_item'>
               <LinkContainer to='/wednesday_pickers'>
                 <Nav.Link>Wednesday</Nav.Link>
               </LinkContainer>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item className='pickers_nav_item'>
               <LinkContainer to='/thursday_pickers'>
                 <Nav.Link>Thursday</Nav.Link>
               </LinkContainer>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item className='pickers_nav_item'>
               <LinkContainer to='/friday_pickers'>
                 <Nav.Link>Friday</Nav.Link>
               </LinkContainer>
