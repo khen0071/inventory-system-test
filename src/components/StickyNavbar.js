cff5536cf3525865327a9bf8928561d2629ab723
khen0071 <71557029+khen0071@users.noreply.github.com>
2022-05-06 04:07:11 +0800

    may5v2

--- src/components/StickyNavbar.js
+++ src/components/StickyNavbar.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { Container, Navbar, Nav, Table } from 'react-bootstrap';
+
 import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
+
 import styles from '../styles/datasheet.module.css';
 const StickyNavbar = () => {
   return (
     <>
-      {/* <Table striped bordered hover responsive className={styles.table_layout1}>
-        <thead className={styles.table_thead}>
-          <tr>
-            {' '}
-            <LinkContainer to='/'>
-              <th>Monday</th>
-            </LinkContainer>
-            <LinkContainer to='/tuesday_pickers'>
-              <th>Tuesday</th>
-            </LinkContainer>
-            <LinkContainer to='/wednesday_pickers'>
-              <th>Wednesday</th>
-            </LinkContainer>
-            <LinkContainer to='/thursday_pickers'>
-              <th>Thursday</th>
-            </LinkContainer>
-            <LinkContainer to='/friday_pickers'>
-              <th>Friday</th>
-            </LinkContainer>
-          </tr>
-        </thead>
-      </Table> */}
       <div className={styles.nav_container}>
         <ul>
           <LinkContainer to='/'>
